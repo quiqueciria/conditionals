96ed6f2b8fc8446756734085aa84a7ad04ca9784
Quique Ciria <quiqueciria@gmail.com>
2023-05-02 11:56:32 +0200

    first test

--- src/main.js
+++ src/main.js
@@ -1,3 +1,11 @@
 import "./style.css";
 
-console.log("Hello from main");
+const haAcertadoElNumero = (numeroAAcertar, numeroDelUsuario) => {
+  if (numeroAAcertar === numeroDelUsuario) {
+    console.log("El usuario ha acertado");
+  } else {
+    console.log("No es el número, prueba otra vez");
+  }
+};
+console.log(haAcertadoElNumero(50, 50)); // Mensaje de consola: El usuario ha acertado;
+console.log(haAcertadoElNumero(50, 51));
